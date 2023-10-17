9561d4a60e6a36f1dc6d237509a290dba5cde4f9
aadarsha2057 <aadarshadhungel2057@gmail.com>
2023-10-16 19:50:31 -0500

    Initial Setup

---
+++ src/main/java/com/utd/edu/cs4389/cometBank/CometBankApplication.java
@@ -0,0 +1,13 @@
+package com.utd.edu.cs4389.cometBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CometBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CometBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utd/edu/cs4389/cometBank/CometBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utd.edu.cs4389.cometBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CometBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
