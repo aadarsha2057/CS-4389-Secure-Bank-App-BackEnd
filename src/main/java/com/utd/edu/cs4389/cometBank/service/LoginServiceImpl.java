4295403656ace456ba111e2a90563cf26a398450
Romez35 <romiepatel@gmail.com>
2023-11-01 16:16:41 -0500

    Password Encyption

--- src/main/java/com/utd/edu/cs4389/cometBank/service/LoginServiceImpl.java
+++ src/main/java/com/utd/edu/cs4389/cometBank/service/LoginServiceImpl.java
@@ -11,6 +11,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 
 @Slf4j
 @Component
@@ -18,6 +21,9 @@ import java.nio.file.StandardOpenOption;
 public class LoginServiceImpl implements LoginService {
 
     private Long temp = 1L;
+    
+    // Add a secret key for encryption
+    private static final String SECRET_KEY = "YourSecretKey123"; // Change this to a secure value
 
     // Default constructor
     public LoginServiceImpl() {
@@ -36,10 +42,13 @@ public class LoginServiceImpl implements LoginService {
             while ((line = br.readLine()) != null) {
                 String[] splitStr = line.split("\\|");
                 if (splitStr[0].equals(loginDTO.getFullName()) &&
-                    splitStr[1].equals(loginDTO.getPassword()) &&
-                    splitStr[2].equals(loginDTO.getEmail())) {
-                    found = true;
-                    break;
+                        splitStr[2].equals(loginDTO.getEmail())) {
+                    // Decrypt stored password for comparison
+                    String storedPassword = decrypt(splitStr[1]);
+                    if (storedPassword.equals(loginDTO.getPassword())) {
+                        found = true;
+                        break;
+                    }
                 }
             }
             // Return true if credentials are found, otherwise return false
@@ -56,7 +65,8 @@ public class LoginServiceImpl implements LoginService {
     @Override
     public void createUser(LoginDTO loginDTO) {
         // Create a string to append to the credentials file
-        String textToAppend = loginDTO.getFullName() + "|" + loginDTO.getPassword() + "|" + loginDTO.getEmail() + "|0" + "\n";
+        String encryptedPassword = encrypt(loginDTO.getPassword());
+        String textToAppend = loginDTO.getFullName() + "|" + encryptedPassword + "|" + loginDTO.getEmail() + "|0" + "\n";
         Path path = Paths.get("creds.txt");
         try {
             // Append the new user information to the credentials file
@@ -65,5 +75,31 @@ public class LoginServiceImpl implements LoginService {
             // Log the exception
             log.error("Error while creating a new user.", e);
         }
+    }
+
+    // Encryption method
+    private String encrypt(String strToEncrypt) {
+        try {
+            SecretKeySpec secretKey = new SecretKeySpec(SECRET_KEY.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes()));
+        } catch (Exception e) {
+            log.error("Error while encrypting password.", e);
+        }
+        return null;
+    }
+
+    // Decryption method
+    private String decrypt(String strToDecrypt) {
+        try {
+            SecretKeySpec secretKey = new SecretKeySpec(SECRET_KEY.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+        } catch (Exception e) {
+            log.error("Error while decrypting password.", e);
+        }
+        return null;
     }
 }
